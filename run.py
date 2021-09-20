e2c38f64e7d91c4c7c3dd5de3c3ea07a8256a6bb
LaurePiechaczyk <laure.piecha@gmail.com>
2021-09-20 11:16:52 +0000

    Change names variables and functions to have the code clearer

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ class Board:
         print(f'\n{self.name} board')
         print ('\n'.join(' '.join(row) for row in self.board))  
 
+
 class Player(Board):
     """
     """
@@ -30,7 +31,8 @@ class Player(Board):
         self.type = type
         Board.__init__(self,type)
 
-def choose_grid(num_rows, num_column, number_boats ):
+
+def player_game_customisation (num_rows, num_column, number_boats):
     while True:   
         try:
             num_rows = int(input("type your number of rows:\n"))
@@ -55,97 +57,112 @@ def choose_grid(num_rows, num_column, number_boats ):
         except ValueError:
             print("Values have to be integers.")
 
-def place_data_in_board (array_to_populate, what_to_place, what_to_write):
-    for i in range(len(what_to_place)):
-        array_to_populate[what_to_place[i]-1] = what_to_write
 
-def show_games_settings():
+def place_data_in_board (array_to_populate, data_to_place, data_Character):
+    for i in range(len(data_to_place)):
+        array_to_populate[data_to_place[i]-1] = data_Character
+
+
+def show_game_settings():
     print("-" * 55)
     print(f"The settingds are: \nNumber of rows = {num_rows} \nNumber of columns = {num_column} \nNumber of ships: {number_boats}")
     print("Top left corner is row: 1, col: 1")
     print("-" * 55)
 
-def show_score():
+
+def show_scores():
     print("-" * 55)
-    print(f'The score is:\n{name}:{len(computer.touched_boats_position)} \nComputer:{len(player.touched_boats_position)} ')
+    player_score = len(computer.touched_boats_position)
+    computer_score = len(player.touched_boats_position)
+    print(f'The score is:\n{name}:{player_score} \nComputer:{computer_score} ')
     print("-" * 55)
 
-def input_player_guess():
+
+def let_player_guess_row_column():
     while True:   
         try:
             player_row_choice = int(input("Enter a row number:\n"))
             player_column_choice = int(input("Enter a column number:\n"))
-            if player_row_choice in range(1,num_rows+1)  and player_column_choice in range(1,num_column+1):
+            if player_row_choice in range(1,num_rows+1) and player_column_choice in range(1,num_column+1):
                 break
             else:
                 print("\nPlease enter correct values")
-                show_games_settings()
+                show_game_settings()
 
         except ValueError:
             print("\nPlease enter correct values")
-            show_games_settings()
-    return player_row_choice,player_column_choice
+            show_game_settings()
+    return player_row_choice, player_column_choice
 
-def make_guess():
+
+def player_guess():
     print("\nYour turn to play")
-    player_row_choice, player_column_choice = input_player_guess()
-    player_choice = player_column_choice + (num_column * (player_row_choice-1))
-    
-    while player_choice in computer.touched_boats_position or player_choice in computer.missed_boats_position :
+    player_row_choice, player_column_choice = let_player_guess_row_column()
+    player_choice_in_array = player_column_choice + (num_column * (player_row_choice-1))
+    player_previous_trials = computer.touched_boats_position + computer.missed_boats_position
+
+
+    while player_choice_in_array in player_previous_trials:
         print("You have already tried this coordinates")
-        player_row_choice, player_column_choice = input_player_guess()
-        player_choice = player_column_choice + (num_column * (player_row_choice-1))
+        player_row_choice, player_column_choice = let_player_guess_row_column()
+        player_choice_in_array = player_column_choice + (num_column * (player_row_choice-1))
 
     print(f'{name} guessed: ({player_row_choice };{player_column_choice})')
-    return player_choice
+    return player_choice_in_array
+
 
 def computer_guess():
     print("\nComputer turn to play")
-    computer_choice = random.randint(1, available_points) #with random.randint we don't add +1 at available_points as for random.sample because with random.randint the last number can be choosen
+    computer_choice_in_array = random.randint(1, available_points) #with random.randint we don't add +1 at available_points as for random.sample because with random.randint the last number can be choosen
+    computer_previous_trials = player.touched_boats_position + player.missed_boats_position
 
-    while computer_choice in player.touched_boats_position or computer_choice in player.missed_boats_position :
-        computer_choice = random.randint(1, available_points) #with random.randint we don't add +1 at available_points as for random.sample because with random.randint the last number can be choosen
+    while computer_choice_in_array in computer_previous_trials:
+        computer_choice_in_array = random.randint(1, available_points) #with random.randint we don't add +1 at available_points as for random.sample because with random.randint the last number can be choosen
 
-    #get the corresponding row and column
-    computer_row_choice = np.where(matrix_game == computer_choice)[0][0]+1
-    computer_column_choice = np.where(matrix_game == computer_choice)[1][0]+1
+    #get the corresponding row and column in matrix to display in the board
+    computer_row_choice = np.where(matrix_game == computer_choice_in_array)[0][0]+1
+    computer_column_choice = np.where(matrix_game == computer_choice_in_array)[1][0]+1
     print(f'Computer guessed: ({computer_row_choice};{computer_column_choice})')
-    return computer_choice
+    return computer_choice_in_array
 
 def check_if_winner():
-    if len(computer.touched_boats_position) == number_boats or len(player.touched_boats_position) == number_boats:
-        if len(computer.touched_boats_position) > len(player.touched_boats_position):
-            show_score()
+    player_score = len(computer.touched_boats_position)
+    computer_score = len(player.touched_boats_position)
+    if player_score == number_boats or computer_score == number_boats:
+        if player_score > computer_score:
+            show_scores()
             print("\nYou won")
             quit()
         else:
-            show_score()
+            show_scores()
             print("\nComputer won")
             quit()
 
-def check_guess(choice,boats_position,touched_boats_position,missed_boats_position):
-    if choice in boats_position:
+def check_if_ship_touched_missed(guess,boats_position,touched_boats_position,missed_boats_position):
+    if guess in boats_position:
         print("A ship has been touched")
-        return touched_boats_position.append(choice)
+        return touched_boats_position.append(guess)
     else:
         print("ships have been missed this time")
-        return missed_boats_position.append(choice)
+        return missed_boats_position.append(guess)
 
-def one_run():
-    check_guess(make_guess(),computer.boats_position,computer.touched_boats_position,computer.missed_boats_position)
+def run_game():
+    check_if_ship_touched_missed(player_guess(),computer.boats_position,computer.touched_boats_position,computer.missed_boats_position)
     check_if_winner()
 
-    check_guess(computer_guess(),player.boats_position,player.touched_boats_position,player.missed_boats_position)
+    check_if_ship_touched_missed(computer_guess(),player.boats_position,player.touched_boats_position,player.missed_boats_position)
     check_if_winner()
 
-    show_score()
+    show_scores()
 
-    next_round_start = (input("Click Enter to continue or n + Enter to quit\n"))
+    next_round_start = (input("Do you want to continue? \nClick on ANY KEY to continue \nto quit click n + Enter \n"))
     if next_round_start in ["N", "n","No", "no"]:
         quit()
     player.print_board()
     computer.print_board()
+    run_game()
 
+# --------------------- Game Start here 
 
 # present the game and welcome the player
 print("-" * 55)
@@ -154,24 +171,30 @@ print("-" * 55)
 
 name = input("Please enter your name:\n")
 
-######start with customization
+# Set default values for board size and number of ships
 num_column = 5
 num_rows = 4
 number_boats = 6
 
+# If player wants, let him/her choose values for board size and number of ships
 constumisation = input (f'Hello {name}, do you want to choose your number of ships and grid size? \nClick Y + Enter \nOtherwise click Enter to play with the default\n' )
-
 if constumisation in ["Y", "y","Yes", "yes"]:
-    num_rows, num_column, number_boats = choose_grid(num_rows, num_column , number_boats)
+    num_rows, num_column, number_boats = player_game_customisation(num_rows, num_column , number_boats)
 
-show_games_settings()
+# Show the player the settings so he/her can understand that row and column start at 1, number of ships
+show_game_settings()
 
+# Setting some variables. 
+# The available points are then in and array and also in a matrix in order to show the board
 available_points = num_column * num_rows
 array_game = np.arange(1,available_points+1) 
 matrix_game = np.reshape(array_game, (num_rows, num_column))
 
+
+# Using the classes to set a player and a computer
 player = Player(name,"player")
 computer = Player("computer","computer") 
+
 
 # first time to show the grids
 print("-" * 55)
@@ -180,7 +203,6 @@ print("-" * 55)
 player.print_board()
 computer.print_board()
 
-while number_boats > len(computer.touched_boats_position) and number_boats > len(player.touched_boats_position):
-    one_run()
+run_game()
 
- 
+
